8764c5b948434f1b8c769c8351fac005461aca13
mrBidone <ab.bz@ukr.net>
2024-04-27 13:10:19 +0200

    Add 1 task

---
+++ js/module-4.js
@@ -0,0 +1,36 @@
+//Напишіть функцію logItems(array), яка приймає
+//масив та використовує цикл for, який для кожного
+//елемента масиву виводитиме повідомлення у форматі
+//<номер елемента> - <значення елемента>
+//Нумерація елементів має починатися з першого.
+//['Джаз', 'Блюз', 'Рок-н-рол', 'Реггі', 'Реп']
+
+// function logItems(array) {
+//     for (let i = 0; i <array.length)
+// }
+
+//Напишіть функцію caclculateAverage()
+//яка приймає довільну кількість
+//аргументів і повертає їхнє середнє значення.
+//Додати перевірку, що аргументи це числа.
+
+function caclculateAverage(...args) {
+    console.log(args);
+
+    let sumCalculate = 0;
+    let count = 0;
+
+    for (let arg of args) {
+        if(typeof arg === "number") {
+                sumCalculate += arg;
+            console.log(sumCalculate);
+            count += 1;
+        }
+
+    } 
+
+    return sumCalculate / count;
+
+}
+
+console.log(caclculateAverage(5,10,15))

53d1def7e21ebda31a7f09b3d405494e5fe52c8f
mrBidone <ab.bz@ukr.net>
2024-05-02 11:18:47 +0200

    1

---
+++ js/call-backs.js
@@ -0,0 +1,21 @@
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+function makeMessage(pizzaName, callback) {
+
+    return ;
+
+}
+
+
+
+
+console.log(makeMessage("Royal Grand", makePizza));
+
+
+
--- js/module-4.js
+++ js/module-4.js
@@ -14,23 +14,23 @@
 //аргументів і повертає їхнє середнє значення.
 //Додати перевірку, що аргументи це числа.
 
-function caclculateAverage(...args) {
-    console.log(args);
+// function caclculateAverage(...args) {
+//     console.log(args);
 
-    let sumCalculate = 0;
-    let count = 0;
+//     let sumCalculate = 0;
+//     let count = 0;
 
-    for (let arg of args) {
-        if(typeof arg === "number") {
-                sumCalculate += arg;
-            console.log(sumCalculate);
-            count += 1;
-        }
+//     for (let arg of args) {
+//         if(typeof arg === "number") {
+//                 sumCalculate += arg;
+//             console.log(sumCalculate);
+//             count += 1;
+//         }
 
-    } 
+//     } 
 
-    return sumCalculate / count;
+//     return sumCalculate / count;
 
-}
+// }
 
-console.log(caclculateAverage(5,10,15))
+// console.log(caclculateAverage(5,10,15))
